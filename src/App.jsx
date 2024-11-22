f8ca1bc7346f8e19ee6c3d161c61200e947623dc
MrXgroot <161230118+MrXgroot@users.noreply.github.com>
2024-11-22 17:22:06 +0530

    additional features
    all the bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Chessboard } from "react-chessboard";
 import io from "socket.io-client";
 import "./App.css";
-const socket = io("http://localhost:5000");
+import { Chess } from "chess.js";
+const socket = io("https://chess-backend-1-728q.onrender.com");
 function App() {
   const [gameId, setGameId] = useState(null);
   const [loadGame, setLoadGame] = useState(false);
-  const [fen, setFen] = useState("start");
+  const [fen, setFen] = useState(
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+  );
   const [role, setRole] = useState(null);
   const [isPlayerTurn, setIsPlayerTurn] = useState(true);
   const copyToClipboard = () => {
@@ -29,6 +32,7 @@ function App() {
       setLoadGame(true);
     });
     socket.on("gameCreated", ({ gameId, role }) => {
+      // console.log(gameId);
       setGameId(gameId);
       setRole(role);
       setIsPlayerTurn(role == "white");
@@ -37,17 +41,26 @@ function App() {
     socket.on("roleAssigned", ({ role }) => {
       setRole(role);
       setIsPlayerTurn(role === "white");
-      console.log("role is ", role, "    isPlayerTurn", isPlayerTurn);
+      // console.log("role is ", role, "    isPlayerTurn", isPlayerTurn);
     });
     socket.on("playerJoined", ({ players, gameId }) => {
       setGameId(gameId);
-      console.log("player joined", players, gameId);
+      // console.log("player joined", players, gameId);
     });
 
     socket.on("moveMade", ({ fen }) => {
-      console.log(fen);
+      // console.log("move made", isPlayerTurn);
       setFen(fen);
       setIsPlayerTurn((p) => !p);
+    });
+
+    socket.on("matchClosed", (message) => {
+      setGameId(null);
+      setFen("start");
+      setRole(null);
+      setIsPlayerTurn(true);
+      setLoadGame(false);
+      alert(message);
     });
     return () => socket.off();
   }, []);
@@ -61,16 +74,29 @@ function App() {
 
   //ended this i have to create the make move in server .js
   const onPieceDrop = (sourceSquare, targetSquare) => {
-    console.log(isPlayerTurn);
     if (!isPlayerTurn) {
-      // alert("Wait for your turn..");
+      // console.log("not your turn");
       return false;
     }
-    // console.log("moving");
     const move = { from: sourceSquare, to: targetSquare };
-    socket.emit("makeMove", { gameId, move });
-    // console.log("returned");
+    let result = null;
+    const chess = new Chess(fen);
+    try {
+      result = chess.move(move);
+    } catch (e) {}
+
+    if (result) {
+      setFen(chess.fen());
+      // console.log(isPlayerTurn);
+      socket.emit("makeMove", { gameId, move });
+    } else {
+      return false;
+    }
     return true;
+  };
+  const joinRandom = () => {
+    socket.emit("joinRandom");
+    alert("wait until some one joins the game");
   };
 
   return (
@@ -82,8 +108,11 @@ function App() {
           Join Game
         </button>
       )}
-      {!loadGame && gameId && <label onClick={copyToClipboard}>{gameId}</label>}
 
+      {!loadGame && <button onClick={joinRandom}>join Random</button>}
+      {!loadGame && gameId && (
+        <label onClick={copyToClipboard}>{`click to copy ID`}</label>
+      )}
       {loadGame && (
         <Chessboard
           position={fen}
